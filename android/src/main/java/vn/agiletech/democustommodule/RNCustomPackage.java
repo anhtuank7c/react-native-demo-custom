76d6f9062bf974817dea291715a7e56e44d87b11
Anh Tuan Nguyen <anhtuank7c@hotmail.com>
2018-02-27 23:42:38 +0700

    Limit React Native 0.47.0+

--- android/src/main/java/vn/agiletech/democustommodule/RNCustomPackage.java
+++ android/src/main/java/vn/agiletech/democustommodule/RNCustomPackage.java
@@ -24,11 +24,6 @@ public class RNCustomPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNCustomModule(reactContext));
     }
 
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
